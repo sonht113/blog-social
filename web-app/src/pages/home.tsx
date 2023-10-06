2f9822ac0c7fd42ec4259f4083726d9fac8db88e
trongson <trongson18101999@gmail.com>
2023-10-06 14:47:06 +0700

    feats:(home) add example query api and change theme mode

--- web-app/src/pages/home.tsx
+++ web-app/src/pages/home.tsx
@@ -1,7 +1,45 @@
+import { gql } from '@apollo/client';
+import { useQuery } from '@tanstack/react-query';
+import request from 'graphql-request';
+
+import { useThemeStore } from '@/hooks';
+
+const QUERY_USERS = gql(`
+    query getUsers($query: OptionsQueryType!){
+        getUsers(query: $query){
+          page
+          limit
+          totalPage
+          data {
+            fullname
+            email
+          }
+        }
+    }
+  `);
+
 const Home = () => {
+  const theme = useThemeStore((state) => state.theme);
+  const setTheme = useThemeStore((state) => state.setTheme);
+
+  const { data } = useQuery({
+    queryKey: ['users'],
+    queryFn: async () =>
+      request('http://localhost:3000/graphql', QUERY_USERS, { query: {} }),
+  });
+
   return (
     <div>
       <span>Home</span>
+      <button
+        onClick={() => {
+          theme === 'dark'
+            ? setTheme({ theme: 'light' })
+            : setTheme({ theme: 'dark' });
+        }}
+      >
+        Change theme
+      </button>
     </div>
   );
 };
