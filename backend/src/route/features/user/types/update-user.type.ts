87d758526a88af511193de7074def71564e0f1dd
trongson <trongson18101999@gmail.com>
2023-10-07 23:07:56 +0700

    fix:(update-user-type) fix update user type with partial type

--- backend/src/route/features/user/types/update-user.type.ts
+++ backend/src/route/features/user/types/update-user.type.ts
@@ -1,38 +1,5 @@
-import { Field, InputType } from '@nestjs/graphql';
-import { IsOptional, IsEmail, IsEmpty } from 'class-validator';
+import { InputType, PartialType } from '@nestjs/graphql';
+import { CreateUserType } from './create-user.type';
 
 @InputType()
-export class UpdateUserType {
-  @IsOptional()
-  @Field({ nullable: true })
-  fullname?: string;
-
-  @IsEmpty()
-  @Field({ nullable: true })
-  role: number;
-
-  @IsOptional()
-  @IsEmail()
-  @Field({ nullable: true })
-  email: string;
-
-  @IsOptional()
-  @Field({ nullable: true })
-  dayOfBirth: string;
-
-  @IsOptional()
-  @Field({ nullable: true })
-  avatar: string;
-
-  @IsOptional()
-  @Field({ nullable: true })
-  phoneNumber: string;
-
-  @IsOptional()
-  @Field({ nullable: true })
-  desc: string;
-
-  @IsOptional()
-  @Field({ nullable: true })
-  address: string;
-}
+export class UpdateUserType extends PartialType(CreateUserType) {}
