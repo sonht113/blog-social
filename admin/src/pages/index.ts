ef071cd9c9366dd59646e812cf6186c639c7d47f
trongson <trongson18101999@gmail.com>
2023-10-10 22:49:01 +0700

    feats: features add login page

--- admin/src/pages/index.ts
+++ admin/src/pages/index.ts
@@ -1,7 +1,8 @@
 import { lazy } from 'react';
 
 const Dashboard = lazy(() => import('@/pages/dashboad'));
+const Login = lazy(() => import('@/pages/login'));
 
 const Users = lazy(() => import('@/pages/users'));
 
-export { Dashboard, Users };
+export { Dashboard, Users, Login };
---
+++ admin/src/pages/login.tsx
@@ -0,0 +1,5 @@
+const Login = () => {
+  return <div className="text-red-500">Login</div>;
+};
+
+export default Login;
