37ee802af7dca1ad29785d59f0f9f99523943b5e
trongson <trongson18101999@gmail.com>
2023-10-11 20:50:11 +0700

    update: update check token for private router

--- admin/src/routes/private-router.tsx
+++ admin/src/routes/private-router.tsx
@@ -1,19 +1,16 @@
 import { ReactElement } from 'react';
 
-import { Button, Result } from 'antd';
-import { Navigate, useNavigate } from 'react-router-dom';
-import { useReadLocalStorage } from 'usehooks-ts';
+import { Navigate } from 'react-router-dom';
 
-import { DASHBOARD_PATH, LOGIN_PATH } from '@/data';
+import { LOGIN_PATH } from '@/data';
 import { useAuthStore } from '@/features/auth';
-import { hasItemInList } from '@/utils';
 
 type Props = {
   children: ReactElement;
 };
 
 const PrivateRoute = ({ children }: Props) => {
-  const token = undefined;
+  const token = useAuthStore((state) => state.token);
 
   return token ? <>{children}</> : <Navigate to={LOGIN_PATH} replace />;
 };
