d94f8d66be08a0aafb9b966c0726d3fd20358452
trongson <trongson18101999@gmail.com>
2023-10-13 00:40:42 +0700

    feats:(path) add profile path

--- web-app/src/data/constant/path.ts
+++ web-app/src/data/constant/path.ts
@@ -2,3 +2,5 @@ export const LOGIN_PATH = '/login';
 export const SIGN_UP_PATH = '/sign-up';
 export const HOME_PATH = '/home';
 export const USER_PATH = '/users';
+
+export const PROFILE_PATH = '/profile';
