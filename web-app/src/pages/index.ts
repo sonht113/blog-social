edf9a9cf432ce2092d62f68825cabeef471a516b
trongson <trongson18101999@gmail.com>
2023-10-12 19:54:27 +0700

    feats: add router for login page and sign up page

--- web-app/src/pages/index.ts
+++ web-app/src/pages/index.ts
@@ -4,4 +4,7 @@ const Home = lazy(() => import('@/pages/home'));
 
 const Users = lazy(() => import('@/pages/users'));
 
-export { Home, Users };
+const Login = lazy(() => import('@/pages/login'));
+const SignUp = lazy(() => import('@/pages/sign-up'));
+
+export { Home, Users, Login, SignUp };
