e2df833b03b0f5e54da024a81d4ab8a7468b914a
trongson <trongson18101999@gmail.com>
2023-10-12 19:55:11 +0700

    chore: remove query provider and axios client

--- web-app/src/main.tsx
+++ web-app/src/main.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
 import { ApolloClientProvider } from './provider/apollo-client-provider.tsx';
-import QueryProvider from './provider/query-provider.tsx';
 import LayoutConfigProvider from './provider/theme-config-provider.tsx';
 import Routes from './routes/index.tsx';
 import { ToasterConfig } from '@/components';
@@ -14,11 +12,8 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <LayoutConfigProvider>
       <ApolloClientProvider>
-        <QueryProvider>
-          <ToasterConfig />
-          <Routes />
-          <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
-        </QueryProvider>
+        <ToasterConfig />
+        <Routes />
       </ApolloClientProvider>
     </LayoutConfigProvider>
   </React.StrictMode>,
