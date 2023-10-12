e29adb60ad790531aa4377fe7ed0f5f9c522c713
trongson <trongson18101999@gmail.com>
2023-10-12 19:50:41 +0700

    feats: add router for login page and sign up page

--- web-app/src/routes/render-router.tsx
+++ web-app/src/routes/render-router.tsx
@@ -2,12 +2,22 @@ import { FC, lazy } from 'react';
 
 import { Navigate, useRoutes } from 'react-router-dom';
 
+import { LOGIN_PATH, SIGN_UP_PATH } from '@/data';
 import { routeList } from '@/data/constant/navs';
 import LayoutComponent from '@/layout';
+import { Login, SignUp } from '@/pages';
 
 const NotFound = lazy(() => import('@/pages/not-found'));
 
 const routes = [
+  {
+    path: LOGIN_PATH,
+    element: <Login />,
+  },
+  {
+    path: SIGN_UP_PATH,
+    element: <SignUp />,
+  },
   {
     path: '/',
     element: <LayoutComponent />,
