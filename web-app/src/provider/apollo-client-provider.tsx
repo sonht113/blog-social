153f83039a0989ec79f2126dac9e9603ac5534ab
trongson <trongson18101999@gmail.com>
2023-10-12 19:51:33 +0700

    feats:(apollo-client-providerĐ set up config header request

--- web-app/src/provider/apollo-client-provider.tsx
+++ web-app/src/provider/apollo-client-provider.tsx
@@ -1,13 +1,35 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { FC, ReactNode } from 'react';
 
-import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 type Props = {
   children: ReactNode;
 };
 
-const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: import.meta.env.VITE_API_URL,
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token');
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ' ',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
