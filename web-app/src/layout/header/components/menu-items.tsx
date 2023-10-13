4100d0e2db6df243b5f377b3122c46068318b597
trongson <trongson18101999@gmail.com>
2023-10-13 20:45:22 +0700

    fix:(menu-item) fix color text of menu item

--- web-app/src/layout/header/components/menu-items.tsx
+++ web-app/src/layout/header/components/menu-items.tsx
@@ -1,36 +1,20 @@
 import { FC } from 'react';
-import { ReactNode } from 'react';
 
 import { Link } from 'react-router-dom';
 
-import { useThemeStore } from '@/hooks';
-
 type Props = {
   text?: string;
-  iconLeft?: ReactNode;
-  iconRight?: ReactNode;
   className?: string;
-  path?: string;
+  path: string;
 };
 
-const MenuItem: FC<Props> = ({
-  text,
-  iconLeft,
-  iconRight,
-  className,
-  path,
-}) => {
-  const theme = useThemeStore((state) => state.theme);
+const MenuItem: FC<Props> = ({ text, className, path }) => {
   return (
-    <Link to={path as string}>
+    <Link to={path}>
       <button
         className={`ease-in duration-300 border-2 border-transparent py-1.5 px-2.5 rounded-xl hover:border-2 hover:border-cyan-500 ${className}`}
       >
-        <span>{iconLeft}</span>
-        <span className={theme === 'dark' ? 'text-white' : 'text-black'}>
-          {text}
-        </span>
-        <span>{iconRight}</span>
+        <span>{text}</span>
       </button>
     </Link>
   );
