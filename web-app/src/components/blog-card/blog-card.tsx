cbdb0591dfa30e62b7f5a22c258ff3260c49dda8
trongson <trongson18101999@gmail.com>
2023-10-14 16:33:58 +0700

    fix:(blog card) fix like and responsive

--- web-app/src/components/blog-card/blog-card.tsx
+++ web-app/src/components/blog-card/blog-card.tsx
@@ -1,9 +1,10 @@
-import { FC, useMemo } from 'react';
+import { FC, useMemo, useState } from 'react';
 
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import ChatIcon from '@mui/icons-material/Chat';
 import FolderCopyIcon from '@mui/icons-material/FolderCopy';
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import ThumbUpAltOutlinedIcon from '@mui/icons-material/ThumbUpAltOutlined';
 import {
   Card,
   CardMedia,
@@ -13,6 +14,7 @@ import {
   Button,
   Box,
 } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 import { CommonButton } from '../common/button/common-button';
 
@@ -24,7 +26,8 @@ type Props = {
   shortDesc: string;
   time: string;
   comments?: number;
-  like?: number;
+  quantityLike?: number;
+  isLiked?: boolean;
 };
 
 type CardActionProps = Partial<Pick<Props, 'className' | 'position'>>;
@@ -36,8 +39,15 @@ export const BlogCard: FC<Props> = ({
   shortDesc,
   time,
   comments = 0,
-  like,
+  quantityLike = 0,
+  isLiked = false,
 }) => {
+  const [liked, setLiked] = useState(isLiked);
+
+  const handleLike = () => {
+    setLiked((prevState) => !prevState);
+  };
+
   const cardMediaStyle = useMemo(
     () => ({
       vertical: {
@@ -46,97 +56,113 @@ export const BlogCard: FC<Props> = ({
       above: {
         height: 340,
       },
-      horizontal: { height: 200, width: 200 },
+      horizontal: { height: 200, width: 300 },
     }),
     [],
   );
 
-  const cardInfo = useMemo(
-    () => (
+  const cardInfo = useMemo(() => {
+    const data = [
+      {
+        icon: <CalendarMonthIcon fontSize="small" />,
+        title: time,
+      },
+      {
+        icon: (
+          <Link to={'/blog/123'}>
+            <ChatIcon fontSize="small" />
+          </Link>
+        ),
+        title: comments,
+      },
+      {
+        icon: liked ? (
+          <ThumbUpIcon fontSize="small" onClick={handleLike} />
+        ) : (
+          <ThumbUpAltOutlinedIcon fontSize="small" onClick={handleLike} />
+        ),
+        title: quantityLike,
+      },
+    ];
+
+    return (
       <Box
         display="flex"
         alignItems="center"
-        gap={3}
+        gap={2}
         className={`mb-4 text-xs ${position === 'vertical' && 'ml-3'}`}
       >
-        <Box display="flex" alignItems="center" gap={1}>
-          <CalendarMonthIcon />
-          <Typography>{time}</Typography>
-        </Box>
-        <Box
-          display="flex"
-          alignItems="center"
-          gap={1}
-          className="cursor-pointer"
-        >
-          <ChatIcon />
-          <Typography>{comments}</Typography>
-        </Box>
-        <Box
-          display="flex"
-          alignItems="center"
-          gap={1}
-          className="cursor-pointer"
-        >
-          <ThumbUpIcon />
-          <Typography>{like}</Typography>
-        </Box>
+        {data.map((d) => (
+          <Box
+            key={d.title}
+            display="flex"
+            alignItems="center"
+            gap={1}
+            className="cursor-pointer"
+          >
+            {d.icon}
+            <Typography>{d.title}</Typography>
+          </Box>
+        ))}
       </Box>
-    ),
-    [comments, time, position, like],
-  );
+    );
+  }, [time, comments, liked, quantityLike, position]);
 
   return (
-    <div>
-      <Card className="relative">
-        <Box display={position === 'horizontal' ? 'flex' : ''}>
+    <Card
+      className={`relative ${
+        position === 'vertical' ? 'max-h-[400px]' : ''
+      } h-full`}
+    >
+      <Box
+        display={position === 'horizontal' ? 'flex' : ''}
+        justifyContent="space-between"
+        className={`h-full ${position === 'vertical' ? 'flex flex-col' : ''}`}
+      >
+        <Box className="relative">
           <CardMedia
             sx={{ ...cardMediaStyle[position] }}
             image={thumbnail}
             title="green iguana"
-            className={position === 'above' ? 'opacity-30' : ''}
           />
-          <CardContent
-            className={
-              position === 'above'
-                ? 'absolute bottom-0'
-                : ' flex flex-col justify-between !py-0'
-            }
-          >
-            {position === 'above' && <CardAction className="p-0" />}
-            <Box>
-              <Typography
-                gutterBottom
-                variant="h5"
-                component="div"
-                className="text-clip line-clamp-1"
-              >
-                {title}
-              </Typography>
-              <Typography
-                variant="body1"
-                color="text.secondary"
-                className="text-clip line-clamp-2 mb-2"
-              >
-                {shortDesc}
-              </Typography>
-            </Box>
-            {position !== 'vertical' && cardInfo}
-          </CardContent>
-          {position !== 'above' && (
-            <CardAction
-              className={`absolute ${
-                position === 'vertical'
-                  ? 'top-40 translate-y-full'
-                  : 'top-[60%] left-1  lg:top-3/4'
-              }`}
-              position={position}
-            />
-          )}
-          {position === 'vertical' && cardInfo}
+          <Box className="w-full h-full absolute top-0 left-0 bg-white dark:bg-black opacity-20 dark:opacity-50"></Box>
         </Box>
-      </Card>
-    </div>
+        <CardContent
+          className={
+            position === 'above'
+              ? 'absolute bottom-0'
+              : 'w-[60%] flex flex-col justify-between !py-0'
+          }
+        >
+          {position === 'above' && <CardAction className="p-0" />}
+          <Box>
+            <Typography
+              gutterBottom
+              variant="h5"
+              component="div"
+              className="text-clip line-clamp-1"
+            >
+              {title}
+            </Typography>
+            <Typography variant="body1" className="text-clip line-clamp-2 mb-2">
+              {shortDesc}
+            </Typography>
+          </Box>
+          {position !== 'vertical' && cardInfo}
+        </CardContent>
+        {position !== 'above' && (
+          <CardAction
+            className={`absolute ${
+              position === 'vertical'
+                ? 'top-40 translate-y-full'
+                : 'top-[60%] left-1  lg:top-3/4'
+            }`}
+            position={position}
+          />
+        )}
+        {position === 'vertical' && cardInfo}
+      </Box>
+    </Card>
   );
 };
 
