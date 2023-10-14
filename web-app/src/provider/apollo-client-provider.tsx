a5f8bb600b31030193e19a48764d456c72bf3551
trongson <trongson18101999@gmail.com>
2023-10-15 03:51:21 +0700

    fix:(apollo-client-provider) fix headers and create upload link

--- web-app/src/provider/apollo-client-provider.tsx
+++ web-app/src/provider/apollo-client-provider.tsx
@@ -1,19 +1,17 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { FC, ReactNode } from 'react';
 
-import {
-  ApolloProvider,
-  ApolloClient,
-  InMemoryCache,
-  createHttpLink,
-} from '@apollo/client';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
+import { createUploadLink } from 'apollo-upload-client';
 
 type Props = {
   children: ReactNode;
 };
 
-const httpLink = createHttpLink({
+const httpLink = createUploadLink({
   uri: import.meta.env.VITE_API_URL,
 });
 
@@ -24,6 +22,7 @@ const authLink = setContext((_, { headers }) => {
     headers: {
       ...headers,
       authorization: token ? `Bearer ${token}` : ' ',
+      'Apollo-Require-Preflight': 'true',
     },
   };
 });
@@ -31,6 +30,7 @@ const authLink = setContext((_, { headers }) => {
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
+  credentials: 'include',
 });
 
 export const ApolloClientProvider: FC<Props> = ({ children }) => {
