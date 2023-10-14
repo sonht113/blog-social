347993127a673a30a9aa53cb3ce77c59a3021b38
trongson <trongson18101999@gmail.com>
2023-10-14 16:32:46 +0700

    fix: fix profile list item

--- web-app/src/layout/header/constant.ts
+++ web-app/src/layout/header/constant.ts
@@ -1,46 +1,9 @@
-import { PROFILE_PATH } from '@/data';
+import { PROFILE_PATH, CREATE_BLOG_PATH } from '@/data';
 
 export const HEADER_LOGO_TEXT = 'BLOG DEV';
 
-export const HEADER_LIST_ITEM = [
-  {
-    id: 1,
-    text: 'Computer',
-    path: '/computer',
-  },
-  {
-    id: 2,
-    text: 'Digital product',
-    path: '/digital-product',
-  },
-  {
-    id: 3,
-    text: 'Electronics',
-    path: '/electronics',
-  },
-  {
-    id: 4,
-    text: 'Cadgets',
-    path: '/cadgets',
-  },
-  {
-    id: 5,
-    text: 'Mobiles',
-    path: '/mobiles',
-  },
-  {
-    id: 6,
-    text: 'Social media',
-    path: '/social-media',
-  },
-  {
-    id: 7,
-    text: 'Technology',
-    path: '/technology',
-  },
-] as const;
-
 export const PROFILE_LIST_ITEM = [
-  { id: 1, name: 'View Profile', path: PROFILE_PATH },
+  { id: 1, name: 'View profile', path: PROFILE_PATH },
+  { id: 2, name: 'Create blog', path: CREATE_BLOG_PATH },
   { id: 2, name: 'Logout' },
 ];
