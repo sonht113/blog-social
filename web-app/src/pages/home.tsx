74dba0d50dd168d88d7306fbf2f1625e1cea091c
trongson <trongson18101999@gmail.com>
2023-10-14 16:34:21 +0700

    fix:(home) fix test blog card

--- web-app/src/pages/home.tsx
+++ web-app/src/pages/home.tsx
@@ -6,37 +6,37 @@ const Home = () => {
   return (
     <div>
       <Grid container spacing={2}>
-        <Grid item xs={12} md={3}>
+        <Grid item xs={12} md={4} lg={3}>
           <BlogCard
-            position="vertical"
+            position="above"
             title="test"
             time="test"
             thumbnail="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
             shortDesc="kim cuong is a handsome boy, he is a student at donga useniversity, you can bla bla bla bla bla bla bla bla bla bl bla bl abl a "
-            like={10}
+            quantityLike={10}
           />
         </Grid>
-        <Grid item xs={12} md={3}>
+        <Grid item xs={12} md={4} lg={3}>
           <BlogCard
-            position="vertical"
+            position="above"
             title="test"
             time="test"
             thumbnail="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
             shortDesc="test"
           />
         </Grid>
-        <Grid item xs={12} md={3}>
+        <Grid item xs={12} md={4} lg={3}>
           <BlogCard
-            position="vertical"
+            position="above"
             title="test"
             time="test"
             thumbnail="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
             shortDesc="test"
           />
         </Grid>
-        <Grid item xs={12} md={3}>
+        <Grid item xs={12} md={4} lg={3}>
           <BlogCard
-            position="vertical"
+            position="above"
             title="test"
             time="test"
             thumbnail="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
