8203192949e9bd902929fea96209c13897ef6a79
trongson <trongson18101999@gmail.com>
2023-10-14 16:33:17 +0700

    feats:(path) update path for blog detail

--- web-app/src/data/constant/path.ts
+++ web-app/src/data/constant/path.ts
@@ -6,4 +6,7 @@ export const SOCIAL_PATH = '/social';
 export const COMPUTER_PATH = '/computer';
 export const SOFTWARE_PATH = '/software';
 
-export const PROFILE_PATH = '/profile';
+export const PROFILE_PATH = '/my-profile';
+export const CREATE_BLOG_PATH = '/create-blog';
+
+export const BLOG_DETAIL = '/blog/:id';
