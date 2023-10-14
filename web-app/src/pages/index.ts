b83c5a8b39aed5f28dacf5b0fa2eb210b62e53b7
kimcuong <nguyenhoangkimcuong02@gmail.com>
2023-10-14 16:00:51 +0700

    feats: setup webapp routes

--- web-app/src/pages/index.ts
+++ web-app/src/pages/index.ts
@@ -1,10 +1,11 @@
 import { lazy } from 'react';
 
 const Home = lazy(() => import('@/pages/home'));
-
 const Users = lazy(() => import('@/pages/users'));
-
 const Login = lazy(() => import('@/pages/login'));
 const SignUp = lazy(() => import('@/pages/sign-up'));
+const Social = lazy(() => import('@/pages/social'));
+const Computer = lazy(() => import('@/pages/computer'));
+const Software = lazy(() => import('@/pages/software'));
 
-export { Home, Users, Login, SignUp };
+export { Home, Users, Login, SignUp, Social, Computer, Software };
---
+++ web-app/src/pages/software.tsx
@@ -0,0 +1,5 @@
+const Software = () => {
+  return <div>Software</div>;
+};
+
+export default Software;
