1063218a3d9f5ad624817e4c83ec4f23a3bb5bbb
trongson <trongson18101999@gmail.com>
2023-10-16 18:30:39 +0700

    feats:(use-blog-query) create hook query popular blog

--- web-app/src/features/blog/hooks/use-blog-query.ts
+++ web-app/src/features/blog/hooks/use-blog-query.ts
@@ -6,7 +6,7 @@ import {
   useQuery,
 } from '@apollo/client';
 
-import { CREATE_BLOG, GET_BLOGS } from '../graphql';
+import { CREATE_BLOG, GET_BLOGS, GET_POPULAR_BLOGS } from '../graphql';
 import {
   DataBlog,
   QueryOption,
@@ -17,6 +17,11 @@ export const useGetBlogsQuery = (query: QueryOption) =>
   useQuery<{ getBlogs: ResPaginationBlogData }, OperationVariables>(GET_BLOGS, {
     variables: { query: query },
   });
+
+export const useGetPopularBlogsQuery = () =>
+  useQuery<{ getPopularBlogs: DataBlog[] }, OperationVariables>(
+    GET_POPULAR_BLOGS,
+  );
 
 export const useCreateBlogMutation = () =>
   useMutation<
