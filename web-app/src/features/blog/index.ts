78fdab84e37b4effe21474fb4d75b10cac80aec5
trongson <trongson18101999@gmail.com>
2023-10-17 20:41:14 +0700

    feats:(enum) add enum category

--- web-app/src/features/blog/index.ts
+++ web-app/src/features/blog/index.ts
@@ -1,4 +1,5 @@
 export * from './services/types';
+export * from './services/enums';
 export * from './hooks/use-blog-query';
 import BlogForm from './components/blog-form';
 
---
+++ web-app/src/features/blog/services/enums.ts
@@ -0,0 +1,5 @@
+export enum EnumCategory {
+  'COMPUTER' = 1,
+  'SOFTWARE' = 2,
+  'SOCIAL' = 3,
+}
