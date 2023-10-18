13144983b033059cb7fa100c110117aec251d6c3
trongson <trongson18101999@gmail.com>
2023-10-19 02:00:41 +0700

    fix: fix import router for login

--- admin/src/pages/index.ts
+++ admin/src/pages/index.ts
@@ -1,8 +1,7 @@
 import { lazy } from 'react';
 
 const Dashboard = lazy(() => import('@/pages/dashboard'));
-const Login = lazy(() => import('@/pages/login'));
 
 const Users = lazy(() => import('@/pages/users'));
 
-export { Dashboard, Users, Login };
+export { Dashboard, Users };
