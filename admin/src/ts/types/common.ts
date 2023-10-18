25b899358d3582ee0ad34d06a6b9fb2206f33868
trongson <trongson18101999@gmail.com>
2023-10-19 01:58:40 +0700

    feats:(ts) add type for custom table

--- admin/src/ts/types/common.ts
+++ admin/src/ts/types/common.ts
@@ -1,4 +1,8 @@
+import { ReactNode } from 'react';
+
 import { QueryKey, UseQueryOptions } from '@tanstack/react-query';
+import { MenuItemType } from 'antd/es/menu/hooks/useItems';
+import { ColumnType } from 'antd/es/table';
 
 export type PageParams = {
   page?: number;
@@ -24,3 +28,47 @@ export type RequiredKeys<T, K extends keyof T> = Required<
   Pick<T, Extract<keyof T, K>>
 > &
   Omit<T, Extract<keyof T, K>>;
+
+export type Pagination = {
+  page?: number;
+  limit?: number;
+};
+
+export type CustomTypeItem = MenuItemType & {
+  type?: string;
+  link?: string;
+  children?: {
+    icon?: unknown;
+    label?: string;
+    key?: string;
+    link?: string;
+  }[];
+};
+
+export type CustomColumnsType<T> = (ColumnType<T> & {
+  initValue?: string[];
+  type?: 'input' | 'select' | 'input-number';
+  options?: { label: string; value: number }[];
+})[];
+
+export type KeyQueryDate<T> = {
+  trackedAt: Partial<T>;
+  sendingDate: Partial<T>;
+  createdDate: Partial<T>;
+};
+
+export type DataIndexQueryDate<T> = {
+  [K in keyof T]: string;
+};
+
+export type BreadcrumbsType = {
+  title: string;
+  link?: string;
+};
+
+export type DescriptionsCustom = {
+  isShow?: boolean;
+  isRequired?: boolean;
+  labelText: string;
+  element: ReactNode;
+};
