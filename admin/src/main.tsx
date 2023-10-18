86fd1e730f10b9586a5de9fb62f910c0f0b8b808
trongson <trongson18101999@gmail.com>
2023-10-19 01:55:32 +0700

    chore: remove tanstack react query and axios

--- admin/src/main.tsx
+++ admin/src/main.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { App as AppProvider } from 'antd';
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
 import './styles/index.less';
 import { ApolloClientProvider } from './provider/apollo-client-provider.tsx';
-import QueryProvider from './provider/query-provider.tsx';
 import LayoutConfigProvider from './provider/theme-config-provider.tsx';
 import Routes from './routes/index.tsx';
 import { ToasterConfig } from '@/components';
@@ -17,11 +15,8 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <LayoutConfigProvider>
       <ApolloClientProvider>
         <AppProvider>
-          <QueryProvider>
-            <ToasterConfig />
-            <Routes />
-            <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
-          </QueryProvider>
+          <ToasterConfig />
+          <Routes />
         </AppProvider>
       </ApolloClientProvider>
     </LayoutConfigProvider>
