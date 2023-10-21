c95200dc4a6787741f1de3d0a5cdcab5a5be2c14
trongson <trongson18101999@gmail.com>
2023-10-21 16:38:21 +0700

    fix:(categories-nav) fix css and fix active

--- web-app/src/components/categories-nav/categories-nav.tsx
+++ web-app/src/components/categories-nav/categories-nav.tsx
@@ -2,16 +2,23 @@ import { Box } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 import SectionTag from '../section-tag';
-import { useQueryCategories } from '@/hooks';
+import { useActiveMenu, useQueryCategories } from '@/hooks';
 
 export const CategoriesNav = () => {
+  const { checkActive } = useActiveMenu();
   const { data } = useQueryCategories();
   return (
     <Box>
       <SectionTag sectionTagName="categories" />
-      <Box className="flex flex-col text-gray-400 ">
+      <Box className="flex flex-col text-gray-400 gap-10">
         {data?.getCategories.map((el) => (
-          <Link to={el.link} key={el.id} className="hover:text-red-600">
+          <Link
+            to={el.link}
+            key={el.id}
+            className={`${
+              checkActive(el.link) ? 'text-red-600' : ''
+            } hover:text-red-600`}
+          >
             {el.name}
           </Link>
         ))}
